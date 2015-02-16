a189be821551c6f552e8699018d414dbf3ecb1fe
William McNichols <wmcnicho@usc.edu>
2015-02-16 15:20:24 +0000

    inital gui commit. Run program from QuantumGuiFrame

---
+++ src/Gui/QuantumGuiFrame.java
@@ -0,0 +1,30 @@
+package Gui;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+public class QuantumGuiFrame extends JFrame {
+	
+	QuantumGuiFrame(){
+		super("Quantum Simulator");
+		setSize(1000,850);
+
+		JMenuBar bar = new QuantumMenuBar();
+		setJMenuBar(bar);
+		
+		
+		QuantumGuiPanel mainPanel = new QuantumGuiPanel();
+		add(mainPanel);
+		
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+		QuantumGuiFrame qFrame = new QuantumGuiFrame();
+		qFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
