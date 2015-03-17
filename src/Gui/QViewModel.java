9ca434489f059a840e1915762ab1cdf74da8ca29
William McNichols <wmcnicho@usc.edu>
2015-03-17 16:06:14 +0000

    Fixed console scrolling. Close #13.

--- src/Gui/QViewModel.java
+++ src/Gui/QViewModel.java
@@ -6,12 +6,15 @@ public class QViewModel{
 
 	public static void printToConsole(String s){
 		QuantumGuiPanel.console.append(s + "\n");
+		QuantumGuiPanel.console.setCaretPosition(QuantumGuiPanel.console.getDocument().getLength());
 	}
 	public static void printToConsole(double d){
 		QuantumGuiPanel.console.append(d + "\n");
+		QuantumGuiPanel.console.setCaretPosition(QuantumGuiPanel.console.getDocument().getLength());
 	}
 	public static void printToConsole(int i){
 		QuantumGuiPanel.console.append(i + "\n");
+		QuantumGuiPanel.console.setCaretPosition(QuantumGuiPanel.console.getDocument().getLength());
 	}
 	public static void updateLoadingBar(int percent){
 		//QuantumGuiPanel.console.append("Change bar to " + percent + "!");
