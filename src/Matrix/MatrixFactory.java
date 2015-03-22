ccba759e1546c5f99353810cd77fb02b370aaa2e
gennarodp <gennarodp@hotmail.de>
2015-03-22 18:34:38 +0000

    comments and cleanup

--- src/Matrix/MatrixFactory.java
+++ src/Matrix/MatrixFactory.java
@@ -2,6 +2,14 @@ package Matrix;
 
 public class MatrixFactory {
 
+	/**
+	 * Creates a matrix size (i,j) of the type specified by the string. Default case is real dense matrix.
+	 * @param i number of rows
+	 * @param j number of columns
+	 * @param type string to specify the type of matrix, possible values are "complex", "sparse", "gate".
+	 * @return	returns an empty matrix of specified size and type.
+	 */
+	@SuppressWarnings("deprecation")
 	public static Matrix create(int i, int j, String type){
 		switch(type){
 		case "complex":
