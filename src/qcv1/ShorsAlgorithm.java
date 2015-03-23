e5277fd7879bf07eee2e69ad1d4434703d5cc4d0
William McNichols <wmcnicho@usc.edu>
2015-03-23 12:50:08 +0000

    Documentation for Shors

--- src/qcv1/ShorsAlgorithm.java
+++ src/qcv1/ShorsAlgorithm.java
@@ -5,6 +5,13 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
 
+/**
+ * An implementation of Shor's algorithm.
+ * The effects of the second register are not completed using a quantum register,
+ * but it makes use of the Quantum Fourier Transform we have implemented.
+ * @author Gennaro
+ *
+ */
 public class ShorsAlgorithm {
 	private int number;
 	private Random rand = new Random();
@@ -14,6 +21,14 @@ public class ShorsAlgorithm {
 	private int x;
 
 	//constructor
+	/**
+	 * Construct the Shor's Algorithm object which requires a number to be passed
+	 * as well as a string to specify what gate representation is used. 
+	 * Checks are in place so to catch numbers without co-prime factors and 
+	 * classical algorithms are used to get much better performance in these cases
+	 * @param rep Gate representation
+	 * @param num The number to be factorised
+	 */
 	public ShorsAlgorithm(String rep, int num){
 		gateRep = rep;
 		number = num;
@@ -23,6 +38,10 @@ public class ShorsAlgorithm {
 
 	}
 
+	/**
+	 * Run this method to return the factors of the number
+	 * @return returns an array containing the factors, [factor 1, factor 2]
+	 */
 	public int [] run(){
 		//choose random int x, 1 <= x <= N-1
 		x = rand.nextInt(number-2) + 1;
@@ -40,19 +59,19 @@ public class ShorsAlgorithm {
 			return null;
 		}
 		int[] out = new int[2];
-		out[0] = ShorsAlgorithm.gcd(y+1, number);
-		out[1] = ShorsAlgorithm.gcd(y-1, number);
+		out[0] = gcd(y+1, number);
+		out[1] = gcd(y-1, number);
 		return out;
 	}
 
-	public static int gcd(int a, int b){
+	protected static int gcd(int a, int b){
 		if (b == 0){
 			return a;
 		}
 		return gcd(b, a % b);
 	}
 
-	public int orderFinding(){
+	protected int orderFinding(){
 		MRegister reg = new MRegister(numOfQubits, "complex");
 		int [] indices = reg2(numOfStates, x, number);
 
@@ -95,7 +114,8 @@ public class ShorsAlgorithm {
 		return -1;
 	}
 
-	public int modularPow(int _base, int _exponent, int _modulus){
+	//finds the solution to x^a mod n
+	protected int modularPow(int _base, int _exponent, int _modulus){
 
 		int modulus = _modulus;
 		int result = 1;
@@ -112,22 +132,25 @@ public class ShorsAlgorithm {
 		return result;
 	}
 
-	public int [] reg2(int noOfStates, int x, int n){
+	//simulates the effects of the second register in Shor's algorithm
+	//returns the indices which the first register should partially collapse onto
+	protected int [] reg2(int noOfStates, int x, int n){
 		int [] states = new int[noOfStates];
 		for(int i=0; i<noOfStates; i++){
 			states[i] = modularPow(x, i, n);
 		}
+		//randomly choose a state that it would collapse onto
 		Random rnd = new Random();
 		int temp = rnd.nextInt(noOfStates-1);
 		temp = states[temp];
 		ArrayList<Integer> list = new ArrayList<Integer>();
-
+		//this corresponds to a set of states in register 1
 		for(int i=0; i<noOfStates; i++){
 			if(states[i]==temp){
 				list.add(new Integer(i));
 			}
 		}
-
+		//return values
 		int[] ret = new int[list.size()];
 		Iterator<Integer> iterator = list.iterator();
 		for (int i = 0; i < ret.length; i++){
